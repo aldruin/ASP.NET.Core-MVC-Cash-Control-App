67bf37219d9ffae2984df24f7127d84a9be75eb4
aldruin <abl.souza97@gmail.com>
2023-12-06 09:02:30 -0300

    Adicionar arquivos de projeto.

---
+++ CashFlowMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CashFlowMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
